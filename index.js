f0e9a44ec9c945cc3c6c1535736dc7694a82dee1
Rene Funcion <rcfun959@gmail.com>
2023-03-16 01:54:36 -0700

    Completed phase-1-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    return Math.abs(pickUpLocation - 42);
+}
+function distanceFromHqInFeet(pickUpLocation) {
+    return 264 * distanceFromHqInBlocks(pickUpLocation);
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+
+
+}
